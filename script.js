3fa167927ec52b419a9b31cbafbbb218608a4ad0
Amin Jalal <mo13241234@gmail.com>
2024-10-14 16:08:21 +0300

    Added JS function to choose the trasaction type adding and removing classes.

---
+++ script.js
@@ -0,0 +1,40 @@
+const totalBalanceLabelNumber = document.getElementById("totalBalanceLabelNumber");
+const totalBalanceLabelText = document.getElementById("totalBalanceLabelText");
+const incomeLabelText = document.getElementById("incomeLabelText");
+const incomeLabelNumber = document.getElementById("incomeLabelNumber");
+const expenseLabelText = document.getElementById("expenseLabelText");
+const expenseLabelNumber = document.getElementById("expenseLabelNumber");
+
+const filterByTypeSelect = document.getElementById("transactionFilterSelect");
+const categoryField = document.getElementById("categoryField");
+
+const transactionsList = document.getElementById("transactionList");
+
+const expenseChoiceButton = document.getElementById("expenseButton");
+const incomeChoiceButton = document.getElementById("incomeButton");
+
+const nameField = document.getElementById("nameField");
+const categorySelect = document.getElementById("categorySelect");
+const amountField = document.getElementById("amountField");
+const dateField = document.getElementById("dateField");
+
+const addExpenseButton = document.getElementById("addExpenseButton");
+
+function chooseTransactionType() {
+    expenseChoiceButton.addEventListener("click", event => {
+        if(incomeChoiceButton.classList.contains('selected')) {
+            incomeChoiceButton.classList.remove('selected');
+            event.target.classList.add('selected');
+        } else {
+            event.target.classList.add('selected');
+        }
+    })
+    incomeChoiceButton.addEventListener("click", event => {
+        if(expenseChoiceButton.classList.contains('selected')) {
+            incomeChoiceButton.classList.remove('selected');
+            event.target.classList.add('selected');
+        } else {
+            event.target.classList.add('selected');
+        }
+    })
+}
