a21455165d576cc6c5889344a0e67027b6c2d02c
Amin Jalal <mo13241234@gmail.com>
2024-10-18 20:12:07 +0300

    Added the functionality to the income, expense and total display, and made them responsive to the number of chars at the display.

--- main/script.js
+++ main/script.js
@@ -1,5 +1,6 @@
 const totalBalanceLabelNumber = document.getElementById("totalBalanceLabelNumber");
 const totalBalanceLabelText = document.getElementById("totalBalanceLabelText");
+const incomeExpenseDiv = document.getElementById('incomeExpense');
 const incomeLabelText = document.getElementById("incomeLabelText");
 const incomeLabelNumber = document.getElementById("incomeLabelNumber");
 const expenseLabelText = document.getElementById("expenseLabelText");
@@ -19,6 +20,14 @@ const amountField = document.getElementById("amountField");
 const dateField = document.getElementById("dateField");
 
 const addExpenseButton = document.getElementById("addExpenseButton");
+
+let income = parseFloat(localStorage.getItem('income')) || 0;
+let expense = parseFloat(localStorage.getItem('expense')) || 0;
+let total = parseFloat(localStorage.getItem('total')) || 0;
+// console.log(total, income, expense);
+expenseLabelNumber.textContent = `-$${expense.toFixed(2)}`;
+incomeLabelNumber.textContent = `+$${income.toFixed(2)}`;
+totalBalanceLabelNumber.textContent = `${total < 0 ? `-$${Math.abs(total).toFixed(2)}` : `+$${total.toFixed(2)}`}`;
 
 let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 let transactionChoice = '';
@@ -77,12 +86,66 @@ function addTransaction() {
         choice: transactionChoice
     };
 
+    if(transactionChoice == 'expense') {
+        expense += transactionObject.amount;
+        expenseLabelNumber.textContent = `-$${expense.toFixed(2)}`;
+        total -= transactionObject.amount;
+        localStorage.setItem('expense', expense);
+        localStorage.setItem('total', total);
+        expenseLabelNumber.textContent = `-$${expense.toFixed(2)}`;
+        totalBalanceLabelNumber.textContent = `${total < 0 ? `-$${Math.abs(total).toFixed(2)}` : `+$${total.toFixed(2)}`}`;
+        adjustFontSize();
+    } else {
+        income += transactionObject.amount;
+        incomeLabelNumber.textContent = `+$${income.toFixed(2)}`;
+        total += transactionObject.amount;
+        localStorage.setItem('income', income);
+        localStorage.setItem('total', total);
+        expenseLabelNumber.textContent = `-$${expense.toFixed(2)}`;
+        totalBalanceLabelNumber.textContent = `${total < 0 ? `-$${Math.abs(total).toFixed(2)}` : `+$${total.toFixed(2)}`}`;
+        adjustFontSize();
+    }
+
     transactions.push(transactionObject);
     localStorage.setItem('transactions', JSON.stringify(transactions));
 
-    // Clear input fields after adding a transaction
+
     nameField.value = '';
     categorySelect.selectedIndex = 0;
     amountField.value = '';
     dateField.value = '';
 }
+setInterval(() => {
+    adjustFontSize();
+}, 100);
+
+// console.log(expenseLabelNumber.textContent.length) 
+// console.log(incomeLabelNumber.textContent.length) 
+
+function adjustFontSize() {
+    incomeLabelNumber.style.fontSize = '2.5rem';
+    expenseLabelNumber.style.fontSize = '2.5rem';
+
+    if (incomeLabelNumber.textContent.length > 6) {
+        incomeLabelNumber.classList.remove('mediumText');
+        incomeExpenseDiv.style.gap = '10%'
+        incomeLabelNumber.style.fontSize = '2.3rem';
+    } else {
+        incomeLabelNumber.classList.add('mediumText');
+    }
+    if (expenseLabelNumber.textContent.length > 6) {
+        expenseLabelNumber.classList.remove('mediumText');
+        expenseLabelNumber.style.fontSize = '2.3rem';
+    } else {
+        expenseLabelNumber.classList.remove('mediumText');
+    }
+    if(totalBalanceLabelNumber.textContent.length > 12) {
+        totalBalanceLabelNumber.classList.remove('bigText');
+        incomeExpenseDiv.style.gap = '10%'
+        totalBalanceLabelNumber.style.fontSize = '3.2rem';
+    } else {
+        totalBalanceLabelNumber.classList.add('bigText');
+    }
+}
+
+// console.log(totalBalanceLabelNumber.textContent.length)
